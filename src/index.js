f0752c0d7f0cc6fe3c92967cbe5aef9a574d4bd8
navsiob <oberoinav1991@gmail.com>
2023-12-25 23:30:37 +0530

    test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Addproducts from "./components/Addproducts";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Addproducts />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
